160c9ed987a0ff96b7c559ef506ef39643173144
abdur-rehman-ali <alijoyia911@gmail.com>
2022-06-02 03:36:50 +0500

    learning 8

---
+++ src/app/test7/test7.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test7',
+  templateUrl: './test7.component.html',
+  styleUrls: ['./test7.component.css']
+})
+export class Test7Component implements OnInit {
+
+  public displayName = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
